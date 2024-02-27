ff7fcc348135224795d135a1ab6165882e7baf16
Michal Gawrys <michal.gawrys@recruitee.com>
2024-02-27 13:49:27 +0100

    create libs

---
+++ libs/lib2/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/lib2/lib2.component';
+import { Lib1Component } from '@nx-move/lib1';
+
+export { Lib1Component };
---
+++ libs/lib2/src/lib/lib2/lib2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nx-move-lib2',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './lib2.component.html',
+  styleUrl: './lib2.component.css',
+})
+export class Lib2Component {}
